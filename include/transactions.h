be5ba542e310f8636baf68ffee638d5be36947d8
Alkatraz16 <golian302@gmail.com>
2026-05-08 19:44:10 +0800

    feat: add transactions header and CRUD function declarations

---
+++ include/transactions.h
@@ -0,0 +1,27 @@
+#ifndef TRANSACTIONS_H
+#define TRANSACTIONS_H
+
+#include <string>
+#include <vector>
+
+struct Transaction {
+    int id;
+    int patientId;
+    std::string date;
+    float amount;
+    std::string serviceType;
+    std::string description;
+};
+
+//menu
+void transactionsManagement();
+
+//CRUD operations
+void addTransaction (std::vector <Transaction>& transactions);
+void viewTransactions (const std::vector <Transaction>& transactions);
+void editTransaction (std::vector <Transaction>& transactions);
+void deleteTransaction (std::vector <Transaction>& transactions);
+
+
+
+#endif
