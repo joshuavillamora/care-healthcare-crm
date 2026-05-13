638868ad2d8ebf0de3f86428f325129f01423c5a
joshuavillamora <earikjoshua@gmail.com>
2026-05-14 06:51:00 +0800

    feat: define Interaction structure for logging patient interactions

--- include/interactions.h
+++ include/interactions.h
@@ -1,6 +1,15 @@
 #ifndef INTERACTIONS_H
 #define INTERACTIONS_H
 
+struct Interaction {
+    int id;
+    int patientId;
+    std::string type;
+    std::string note;
+    std::string date;
+    std::string loggedAt;
+};
+
 void printInteractionsMenu();
 
 #endif
