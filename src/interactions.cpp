f4c5a394411d0774eee793f6389b6325da856c7c
joshuavillamora <earikjoshua@gmail.com>
2026-05-14 13:58:21 +0800

    fix: clear console before displaying interaction logs menu and removed misplaced \

--- src/interactions.cpp
+++ src/interactions.cpp
@@ -10,13 +10,14 @@
 std::vector<Interaction> interactions;
 
 void printInteractionsMenu() {
+    system("cls");
     std::cout << "============================\n";
     std::cout << "      INTERACTION LOGS\n";
     std::cout << "============================\n";
     std::cout << "1. Add Interaction Log\n";
     std::cout << "2. View All Logs\n";
     std::cout << "3. View Logs per Patient\n";
-    std::cout << "4. Delete Log\n";\
+    std::cout << "4. Delete Log\n";
     std::cout << "5. Back to Main Menu\n";
 }
 
