754513ee6d4c2cf0967d97f66665c50dda62d025
chuchuu <omshantilimpin@gmail.com>
2026-05-16 14:44:04 +0800

    feat: add reports module and integrate into main menu

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 void printHomePage();
 void patientManagementModule();
 void interactionsModule();
+void reportsModule();
 
 int main() {
     int choice;
@@ -36,7 +37,7 @@ int main() {
                 interactionsModule();
                 break;
             case 4:
-                std::cout << "This is the Reporting Module.\n";
+                reportsModule();
                 break;
             case 5:
                 std::cout << "Thank you for using Clinical Activity and Records Engine!\n";
